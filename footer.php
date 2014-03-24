eb37da0d2922586932d87d3bf3110ae2646eb427
Ouachita Hills SFTP User <ohsftp@ps261996.dreamhostps.com>
2014-03-24 07:26:08 -0700

    added footer credits link

--- footer.php
+++ footer.php
@@ -6,6 +6,7 @@
 	echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'blankslate' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) );
 ?>
 </div>
+<div class="credits"><a href="<?php echo getURLByTitle('Credits') ?>">Credits</a></div>
 </footer>
 </div>
 <?php wp_footer(); ?>
