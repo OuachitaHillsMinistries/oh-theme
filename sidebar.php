c9ef72996bee8c39598ac355236b4ccf39bc0bdf
Nathan <narthur.a@gmail.com>
2015-05-14 20:02:10 -0500

    Added home & live links to sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,12 @@
 <aside id="sidebar" role="complementary">
     <h3>Navigation</h3>
     <ul class="secondary-nav" id="nav">
+        <li class="home<?php if (is_home() || is_front_page()) { echo ' current_page_item'; } ?>">
+            <a href="<?php echo home_url() ?>">Home</a>
+        </li>
+        <li class="live">
+            <a href="<?php echo get_option('streamingUrl'); ?>">Live</a>
+        </li>
         <?php wp_list_pages(array(
             'title_li'=>null
         )); ?>
