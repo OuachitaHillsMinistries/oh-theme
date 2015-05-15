8127e2c2c1173df5a7dbf19ee45a59b810a5ed7f
Nathan <narthur.a@gmail.com>
2015-05-15 09:56:56 -0500

    Added default search text

--- javascript.js
+++ javascript.js
@@ -13,4 +13,18 @@ jQuery(document).ready(function() {
 			jQuery(this).html('+')
 		}
 	});
+
+	jQuery('#s').value = 'Search';
+	jQuery('#s').focus(function() {
+		if (this.value == 'Search') {
+			this.value = '';
+			jQuery(this).css('color','black');
+		}
+	});
+	jQuery('#s').blur(function() {
+		if (this.value == '') {
+			this.value = 'Search';
+			jQuery(this).css('color','grey');
+		}
+	});
 });
