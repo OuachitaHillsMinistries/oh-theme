f3d1d153a1fc1ff9c068f403f8f0a358a6e06c82
Nathan <narthur.a@gmail.com>
2015-05-15 11:12:17 -0500

    refactored javascript

--- javascript.js
+++ javascript.js
@@ -1,38 +1,43 @@
 // JavaScript Document
 
 jQuery(document).ready(function() {
-	jQuery('.page_item_has_children').append('<a class="toggle" href="#">+</a>');
-	jQuery('.current_page_item>.toggle, .current_page_ancestor>.toggle').html('-');
+	function setupNavigationSubmenus() {
+		jQuery('.page_item_has_children').append('<a class="toggle" href="#">+</a>');
+		jQuery('.current_page_item>.toggle, .current_page_ancestor>.toggle').html('-');
+		jQuery('.toggle').click(function (e) {
+			e.preventDefault();
+			jQuery(this).siblings('ul').slideToggle();
+			if (jQuery(this).html() == '+') {
+				jQuery(this).html('-')
+			} else {
+				jQuery(this).html('+')
+			}
+		});
+	}
 
-	jQuery('.toggle').click(function(e) {
-		e.preventDefault();
-		jQuery(this).siblings('ul').slideToggle();
-		if (jQuery(this).html() == '+') {
-			jQuery(this).html('-')
-		} else {
-			jQuery(this).html('+')
-		}
-	});
+	function setupSearchBoxDefaultText() {
+		var searchBoxes = jQuery("input[name='s']");
+		searchBoxes.each(function () {
+			if (this.value == '') {
+				this.value = 'Search';
+			} else if (this.value != 'Search') {
+				jQuery(this).css('color', 'black');
+			}
+		});
+		searchBoxes.focus(function () {
+			if (this.value == 'Search') {
+				this.value = '';
+				jQuery(this).css('color', 'black');
+			}
+		});
+		searchBoxes.blur(function () {
+			if (this.value == '') {
+				this.value = 'Search';
+				jQuery(this).css('color', 'grey');
+			}
+		});
+	}
 
-
-	var searchBoxes = jQuery("input[name='s']");
-	searchBoxes.each(function() {
-		if (this.value == '') {
-			this.value = 'Search';
-		} else if (this.value != 'Search') {
-			jQuery(this).css('color','black');
-		}
-	});
-	searchBoxes.focus(function() {
-		if (this.value == 'Search') {
-			this.value = '';
-			jQuery(this).css('color','black');
-		}
-	});
-	searchBoxes.blur(function() {
-		if (this.value == '') {
-			this.value = 'Search';
-			jQuery(this).css('color','grey');
-		}
-	});
+	setupNavigationSubmenus();
+	setupSearchBoxDefaultText();
 });
