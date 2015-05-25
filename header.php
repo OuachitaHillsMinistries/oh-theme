3d28a70aac75441d1185652dada25f74dc8cd5d3
Nathan <narthur.a@gmail.com>
2015-05-25 13:58:50 -0500

    Made theme only display site name in title when on landing page

--- header.php
+++ header.php
@@ -3,8 +3,8 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width" />
-	<title><?php wp_title( ' | ', true, 'right' ); ?></title>
 	<?php
+	echo htmlTitle();
 	registerStylesheets();
 	registerScripts();
 	echo landingPageCss();
