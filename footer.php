654a5ec220f1b7204b6d79b04d9a468ff8db2a0c
Nathan Arthur <narthur.a@gmail.com>
2015-11-08 10:20:56 -0600

    Moved credits links into footer

--- footer.php
+++ footer.php
@@ -7,7 +7,13 @@
 		echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'blankslate' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) );
 		?>
 	</div>
-	<div class="credits"><a href="<?php echo getURLByTitle('Credits') ?>">Credits</a></div>
+	<div class="credits">
+        <?php $creditsFolder = get_bloginfo('template_directory') . '/img/credits';?>
+        <a href="#"><img src="<?php echo $creditsFolder; ?>/github-favicon.ico" alt="GitHub" title="GitHub" width=16 height=16 /></a>
+        <a href="#"><img src="<?php echo $creditsFolder; ?>/wordpress-favicon.ico" alt="WordPress" title="WordPress" width=16 height=16 /></a>
+        <a href="#"><img src="<?php echo $creditsFolder; ?>/tidythemes-favicon.ico" alt="TidyThemes" title="TidyThemes" width=16 height=16 /></a>
+        <a href="#"><img src="<?php echo $creditsFolder; ?>/surveymoz-favicon.ico" alt="SurveyMoz" title="SurveyMoz" width=16 height=16 /></a>
+    </div>
 </footer>
 <?php wp_footer(); ?>
 </body>
