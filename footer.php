eb4c41b950df2cc92009f34db5ee904c222d3553
Nathan Arthur <narthur.a@gmail.com>
2015-11-08 10:27:55 -0600

    Linked credit icons to correct URLs

--- footer.php
+++ footer.php
@@ -9,10 +9,10 @@
 	</div>
 	<div class="credits">
         <?php $creditsFolder = get_bloginfo('template_directory') . '/img/credits';?>
-        <a href="#"><img src="<?php echo $creditsFolder; ?>/github-favicon.ico" alt="GitHub" title="GitHub" width=16 height=16 /></a>
-        <a href="#"><img src="<?php echo $creditsFolder; ?>/wordpress-favicon.ico" alt="WordPress" title="WordPress" width=16 height=16 /></a>
-        <a href="#"><img src="<?php echo $creditsFolder; ?>/tidythemes-favicon.ico" alt="TidyThemes" title="TidyThemes" width=16 height=16 /></a>
-        <a href="#"><img src="<?php echo $creditsFolder; ?>/surveymoz-favicon.ico" alt="SurveyMoz" title="SurveyMoz" width=16 height=16 /></a>
+        <a href="https://github.com/OuachitaHillsMinistries/oh-theme"><img src="<?php echo $creditsFolder; ?>/github-favicon.ico" alt="GitHub" title="GitHub" width=16 height=16 /></a>
+        <a href="https://wordpress.org/"><img src="<?php echo $creditsFolder; ?>/wordpress-favicon.ico" alt="WordPress" title="WordPress" width=16 height=16 /></a>
+        <a href="http://tidythemes.com/"><img src="<?php echo $creditsFolder; ?>/tidythemes-favicon.ico" alt="TidyThemes" title="TidyThemes" width=16 height=16 /></a>
+        <a href="http://www.surveymoz.com/"><img src="<?php echo $creditsFolder; ?>/surveymoz-favicon.ico" alt="SurveyMoz" title="SurveyMoz" width=16 height=16 /></a>
     </div>
 </footer>
 <?php wp_footer(); ?>
