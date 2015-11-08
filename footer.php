6879080cff111cbef532b78508b408de4a78c8f7
Nathan Arthur <narthur.a@gmail.com>
2015-11-08 09:30:51 -0600

    Made footer sticky

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
 <div class="clear"></div>
 </div>
+</div>
 <footer id="footer" role="contentinfo">
-<div id="copyright">
-<?php
-	echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'blankslate' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) );
-?>
-</div>
-<div class="credits"><a href="<?php echo getURLByTitle('Credits') ?>">Credits</a></div>
+	<div id="copyright">
+		<?php
+		echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'blankslate' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) );
+		?>
+	</div>
+	<div class="credits"><a href="<?php echo getURLByTitle('Credits') ?>">Credits</a></div>
 </footer>
-</div>
 <?php wp_footer(); ?>
 </body>
 </html>
